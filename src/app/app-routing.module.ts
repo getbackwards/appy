e4743fc36d42cab079435ae04eaa0d2f0b37172c
tgray <tony.gray@trademe.co.nz>
2019-05-05 16:26:22 +1200

    starting routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// import { HomeComponent } from './home/home/home.component';
+
+import { HomeComponent } from './home/home/home.component';
+
+// const routes: Routes = [];
+
+// const routes: Routes = [
+//   { path: 'search', component: SearchComponent },
+//   { path: 'edit/:id', component: EditComponent },
+//   { path: '', redirectTo: '/search', pathMatch: 'full' }
+// ];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
